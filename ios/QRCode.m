d3b26ac219b12a7b97f053f7267b4c533c80565b
Wind Assassin <lavrin110413@gmail.com>
2018-08-18 20:00:44 +0300

    "Contribution: 2018-08-18 20:00"

---
+++ ios/QRCode.m
@@ -0,0 +1,26 @@
+//
+//  RNTQRCode.m
+//  ZapperReact
+//
+//  Created by Keiran van Vuuren on 2018/05/30.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "QRCode.h"
+#import "QRCodeView.h"
+
+@implementation QRCode
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_VIEW_PROPERTY(value, NSString)
+RCT_EXPORT_VIEW_PROPERTY(bgColor, NSString)
+RCT_EXPORT_VIEW_PROPERTY(fgColor, NSString)
+RCT_EXPORT_VIEW_PROPERTY(size, NSNumber)
+
+- (UIImageView *)view
+{
+    return [[QRCodeView alloc] init];
+}
+
+@end
---
+++ ios/QRCodeView.m
@@ -0,0 +1,45 @@
+//
+//  QRCodeView.m
+//  ZapperReact
+//
+//  Created by Keiran van Vuuren on 2018/05/30.
+//  Copyright © 2018 Zapper. All rights reserved.
+//
+
+#import "QRCodeView.h"
+#import "QRCodeGenerator.h"
+
+@implementation QRCodeView
+
+- (void)setValue:(NSString *)value {
+    _value = value;
+    [self generateImage];
+}
+
+- (void)setSize:(NSNumber *)value {
+    _size = value;
+    [self generateImage];
+}
+
+- (void)setBgColor:(NSString *)value {
+    _bgColor = value;
+    [self generateImage];
+}
+
+- (void)setFgColor:(NSString *)value {
+    _fgColor = value;
+    [self generateImage];
+}
+
+- (void)generateImage {
+    if (![_value isEqualToString:@""]) {
+        QRCodeGenerator *qr = [[QRCodeGenerator alloc] initWithString: self.value];
+        qr.size = CGSizeMake([_size doubleValue], [_size doubleValue]);
+        qr.color = [CIColor colorWithRGBA:_fgColor];
+        qr.backgroundColor = [CIColor colorWithRGBA:_bgColor];
+        [self setImage:[qr getImage]];
+    }
+}
+
+
+@end
